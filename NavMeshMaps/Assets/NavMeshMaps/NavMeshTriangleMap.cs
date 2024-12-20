8372e0dfe2f352e4eb7d3e48512070c704c04488
DAIKI <daiki.evilone@gmail.com>
2024-12-21 00:43:49 +0900

    Reimplemented with a structure similar to the Mesh class.

--- NavMeshMaps/Assets/NavMeshMaps/NavMeshTriangleMap.cs
+++ NavMeshMaps/Assets/NavMeshMaps/NavMeshTriangleMap.cs
@@ -9,22 +9,31 @@ namespace NavMeshMaps
         [SerializeField]
         private bool _buildOnAwake = true;
 
-        public class Polygon
+        private List<Vector3> _vertices = new List<Vector3>();
+        public IReadOnlyList<Vector3> Vertices => _vertices;
+
+        public class Triangle
         {
-            public Vector3[] vertices;
+            public int[] indices;
+            public int[] triangleConnections;
             public Vector3 center;
-            public Connection[] connections;
         }
 
-        public class Connection
+        private List<Triangle> _triangles = new List<Triangle>();
+        public IReadOnlyList<Triangle> Triangles => _triangles;
+
+        public class TriangleConnection
         {
-            public int from;
-            public int to;
-            public float length;
+            public int triangleA;
+            public int triangleB;
+            public int overlapedVertexCount;
         }
 
-        private List<Polygon> _polygons = new List<Polygon>();
-        private List<Connection> _connections = new List<Connection>();
+        private List<TriangleConnection> _triangleConnections = new List<TriangleConnection>();
+        public IReadOnlyList<TriangleConnection> TriangleConnections => _triangleConnections;
+
+        private List<int> _verticesTranslationBuffer = new List<int>();
+        private List<int> _triangleConnectionBuffer = new List<int>();
 
         private void Awake()
         {
@@ -38,122 +47,128 @@ namespace NavMeshMaps
         {
             var stopwatch = new System.Diagnostics.Stopwatch();
             stopwatch.Start();
-            _polygons.Clear();
-            _connections.Clear();
 
-            // Calculate polygons
-            var tris = NavMesh.CalculateTriangulation();
-            var indicesCount = tris.indices.Length;
-            for (int i = 0; i <= indicesCount - 3; i += 3)
+            var sourceTris = NavMesh.CalculateTriangulation();
+
+            // Collect and merge vertices
+            _vertices.Clear();
+            _verticesTranslationBuffer.Clear();
+            var sourceVerticesCount = sourceTris.vertices.Length;
+            for (int i = 0; i < sourceVerticesCount; i++)
             {
-                var polygon = new Polygon();
-                polygon.vertices = new Vector3[3] { tris.vertices[tris.indices[i]], tris.vertices[tris.indices[i + 1]], tris.vertices[tris.indices[i + 2]] };
-                var v0 = tris.vertices[tris.indices[i]];
-                var v1 = tris.vertices[tris.indices[i + 1]];
-                var v2 = tris.vertices[tris.indices[i + 2]];
-                polygon.center = (v0 + v1 + v2) / 3;
-                _polygons.Add(polygon);
+                var vertex = sourceTris.vertices[i];
+                var merged = false;
+                for (int j = 0; j < _vertices.Count; j++)
+                {
+                    if (Vector3.SqrMagnitude(vertex - _vertices[j]) < 0.001f)
+                    {
+                        _verticesTranslationBuffer.Add(j);
+                        merged = true;
+                        break;
+                    }
+                }
+                if (!merged)
+                {
+                    _vertices.Add(vertex);
+                    _verticesTranslationBuffer.Add(_vertices.Count - 1);
+                }
             }
 
-            // Calculate connections
-            var polygonCount = _polygons.Count;
+            // Collect triangles
+            _triangles.Clear();
+            var sourceIndicesCount = sourceTris.indices.Length;
+            for (int i = 0; i <= sourceIndicesCount - 3; i += 3)
+            {
+                var idx0 = sourceTris.indices[i];
+                var idx1 = sourceTris.indices[i + 1];
+                var idx2 = sourceTris.indices[i + 2];
+                var triangle = new Triangle();
+                triangle.indices = new int[]
+                {
+                    _verticesTranslationBuffer[idx0],
+                    _verticesTranslationBuffer[idx1],
+                    _verticesTranslationBuffer[idx2],
+                };
+                triangle.center = (_vertices[triangle.indices[0]] + _vertices[triangle.indices[1]] + _vertices[triangle.indices[2]]) / 3;
+                _triangles.Add(triangle);
+            }
+
+            // Calculate triangle connections
+            _triangleConnections.Clear();
+            var polygonCount = _triangles.Count;
             for (int i = 0; i < polygonCount; i++)
             {
-                var currentPoly = _polygons[i];
+                var currentPoly = _triangles[i];
                 for (int j = i + 1; j < polygonCount; j++)
                 {
-                    var targetPoly = _polygons[j];
-                    if (GetSameCount(currentPoly.vertices, targetPoly.vertices) < 1)
+                    var targetPoly = _triangles[j];
+                    var overlapCount = CountSameValues(currentPoly.indices, targetPoly.indices);
+                    if (overlapCount < 1)
                     {
                         continue;
                     }
-
-                    var length = Vector3.Distance(currentPoly.center, targetPoly.center);
-                    _connections.Add(new Connection
+                    _triangleConnections.Add(new TriangleConnection
                     {
-                        from = i,
-                        to = j,
-                        length = length,
-                    });
-                    _connections.Add(new Connection
-                    {
-                        from = j,
-                        to = i,
-                        length = length,
+                        triangleA = i,
+                        triangleB = j,
+                        overlapedVertexCount = overlapCount,
                     });
                 }
             }
-
-            var connectionBuffer = new List<Connection>();
-            for (int i = 0; i < polygonCount; i++)
+            for (int i = 0; i < _triangles.Count; i++)
             {
-                connectionBuffer.Clear();
-                foreach (var connection in _connections)
+                var triangle = _triangles[i];
+                _triangleConnectionBuffer.Clear();
+                for (int j = 0; j < _triangleConnections.Count; j++)
                 {
-                    if (connection.from == i)
+                    var triangleConnection = _triangleConnections[j];
+                    if (triangleConnection.triangleA == i || triangleConnection.triangleB == i)
                     {
-                        connectionBuffer.Add(connection);
+                        _triangleConnectionBuffer.Add(j);
                     }
                 }
-                _polygons[i].connections = connectionBuffer.ToArray();
+                triangle.triangleConnections = _triangleConnectionBuffer.ToArray();
             }
 
-            Debug.Log($"Polygons: {_polygons.Count}, Connections: {_connections.Count}, Elapsed: {stopwatch.ElapsedMilliseconds}ms");
+            Debug.Log($"Polygons: {_triangles.Count}, Connections: {_triangleConnections.Count}, Elapsed: {stopwatch.ElapsedMilliseconds}ms");
         }
 
-        private int GetSameCount(Vector3[] verts1, Vector3[] verts2)
+        private int CountSameValues(int[] values0, int[] values1)
         {
-            var sameVertCount = 0;
-            for (int i = 0; i < verts1.Length; i++)
+            var count = 0;
+            for (int i = 0; i < values0.Length; i++)
             {
-                for (int j = 0; j < verts2.Length; j++)
+                for (int j = 0; j < values1.Length; j++)
                 {
-                    if (verts1[i] == verts2[j])
+                    if (values0[i] == values1[j])
                     {
-                        sameVertCount++;
+                        count++;
                     }
                 }
             }
-            return sameVertCount;
-        }
-
-        public int GetPolygonCount()
-        {
-            return _polygons.Count;
-        }
-
-        public Polygon GetPolygon(int index)
-        {
-            return _polygons[index];
-        }
-
-        public void GetPolygons(List<Polygon> resultPolygons)
-        {
-            resultPolygons.AddRange(_polygons);
-        }
-
-        public void GetConnections(List<Connection> resultConnections)
-        {
-            resultConnections.AddRange(_connections);
+            return count;
         }
 
         private void OnDrawGizmosSelected()
         {
-            Gizmos.color = Color.blue;
-            foreach (var polygon in _polygons)
+            var color1 = new Color(0.0f, 0.0f, 1.0f);
+            var color2 = new Color(0.3f, 0.3f, 1.0f);
+            foreach (var connection in _triangleConnections)
+            {
+                Gizmos.color = connection.overlapedVertexCount >= 2 ? color1 : color2;
+                Gizmos.DrawLine(_triangles[connection.triangleA].center, _triangles[connection.triangleB].center);
+            }
+            Gizmos.color = color1;
+            foreach (var polygon in _triangles)
             {
                 Gizmos.DrawSphere(polygon.center, 0.1f);
-            }
-            foreach (var connection in _connections)
-            {
-                Gizmos.DrawLine(_polygons[connection.from].center, _polygons[connection.to].center);
             }
         }
     }
 
     public static class NavMeshTriUtility
     {
-        public static int GetClosestPolygon(Vector3 position, IReadOnlyList<NavMeshTriangleMap.Polygon> polygons)
+        public static int GetClosestPolygon(Vector3 position, IReadOnlyList<NavMeshTriangleMap.Triangle> polygons)
         {
             var minDist = float.MaxValue;
             var minIndex = -1;
