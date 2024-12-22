5c00a619cc537126efeb92903192a64713533195
DAIKI <daiki.evilone@gmail.com>
2024-12-22 16:54:49 +0900

    Rename class

--- DijkstraNavMesh/Assets/Samples/Scripts/Chaser.cs
+++ DijkstraNavMesh/Assets/Samples/Scripts/Chaser.cs
@@ -1,12 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace NavMeshMapsSample
+namespace DijkstraNavMeshSample
 {
     public class Chaser : MonoBehaviour
     {
         [SerializeField]
-        private CostMapContainer _costMapContainer = null;
+        private DijkstraCostGraphContainer _costGraphContainer = null;
 
         [SerializeField]
         private float _speed = 1.0f;
@@ -19,20 +19,20 @@ namespace NavMeshMapsSample
         {
             if (!_hasDestination)
             {
-                var currentIndex = _costMapContainer.CostMap.GetClosestNodeIndex(transform.position);
-                _costMapContainer.CostMap.GetConnectedNodes(currentIndex, _connectedNodeBuffer);
+                var currentIndex = _costGraphContainer.CostGraph.GetClosestNodeIndex(transform.position);
+                _costGraphContainer.CostGraph.GetConnectedNodes(currentIndex, _connectedNodeBuffer);
                 var minScore = float.MaxValue;
                 var minIndex = currentIndex;
                 foreach (var connectedNode in _connectedNodeBuffer)
                 {
-                    var score = _costMapContainer.CostMap.GetCost(connectedNode);
+                    var score = _costGraphContainer.CostGraph.GetCost(connectedNode);
                     if (score < minScore)
                     {
                         minScore = score;
                         minIndex = connectedNode;
                     }
                 }
-                _destination = _costMapContainer.CostMap.GetPosition(minIndex);
+                _destination = _costGraphContainer.CostGraph.GetPosition(minIndex);
                 _hasDestination = true;
             }
 
